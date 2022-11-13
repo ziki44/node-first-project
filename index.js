73ac5a63bc37f7b25e76c7aeba29ec5a2674c058
Daniel Zgliński <daniel.zglinski@gmail.com>
2022-11-13 10:00:27 +0100

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,5 @@
+//to jest to samo co import fs from 'fs'
+const fs = require('fs')
+// fsp to jest wersja modułu fs oparta na promisach zamiasta na collbackach - poprawia to czytelność 
+const fsp = fs.promises;
+
